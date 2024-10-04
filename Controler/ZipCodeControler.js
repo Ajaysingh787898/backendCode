2ec45d441dabf5c40699a265b59fc08700b935dd
Ajaysingh787898 <ajay.singh@codeviksolutions.com>
2024-10-04 11:13:55 +0530

    update codee

--- Controler/ZipCodeControler.js
+++ Controler/ZipCodeControler.js
@@ -1,49 +1,88 @@
 const ZipCode = require("../model/ZipCode")
-const create = async (req, res) => {
+// const create = async (req, res) => {
+//     try {
+//         const data = new ZipCode(req.body)
+//         console.log("Data is", data);
+
+//         data.save()
+
+//         res.status(200).send({ success: true, message: "zipCode value successfully save " })
+
+//     }
+//     catch (error) {
+//         res.staus(500).send({ success: false, message: "internal server error" })
+//     }
+// }
+
+
+// const update = async (req, res) => {
+//     try {
+
+//         const data = await ZipCode.findOne({ _id: req.params._id })
+//         if (data) {
+//             data.position = req.body.position ?? data.position;
+//             data.heading = req.body.heading ?? data.heading;
+//             data.icon = req.body.icon ?? data.icon;
+//             data.placeholder = req.body.placeholder ?? data.placeholder;
+//             data.borderStyle = req.body.borderStyle ?? data.borderStyle;
+//             data.borderColor = req.body.borderColor ?? data.borderColor;
+//             data.borderRadius = req.body.borderRadius ?? data.borderRadius;
+//             data.buttonBgColor = req.body.buttonBgColor ?? data.buttonBgColor;
+//             data.buttonTextColor = req.body.buttonTextColor ?? data.buttonTextColor;
+//             data.successMessage = req.body.successMessage ?? data.successMessage;
+//             data.successColor = req.body.successColor ?? data.successColor;
+//             data.failureMessage = req.body.failureMessage ?? data.failureMessage;
+//             data.failureColor = req.body.failureColor ?? data.failureColor;
+//             console.log(data);
+//             await data.save();
+//             res.send({ success: true, message: "Record is Updated" });
+//         } else {
+
+//             console.log("data not found");
+//             res.status(404).send({ success: false, message: "Data not found" });
+//         }
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send({ success: false, message: "Internal Server Error" });
+//     }
+// }
+
+
+const createOrUpdate = async (req, res) => {
     try {
-        const data = new ZipCode(req.body)
-        console.log("Data is", data);
+        
+        const existingData = await ZipCode.findOne();
+        
+        if (existingData) {
+            
+            existingData.position = req.body.position ?? existingData.position;
+            existingData.heading = req.body.heading ?? existingData.heading;
+            existingData.icon = req.body.icon ?? existingData.icon;
+            existingData.placeholder = req.body.placeholder ?? existingData.placeholder;
+            existingData.borderStyle = req.body.borderStyle ?? existingData.borderStyle;
+            existingData.borderColor = req.body.borderColor ?? existingData.borderColor;
+            existingData.borderRadius = req.body.borderRadius ?? existingData.borderRadius;
+            existingData.buttonBgColor = req.body.buttonBgColor ?? existingData.buttonBgColor;
+            existingData.buttonTextColor = req.body.buttonTextColor ?? existingData.buttonTextColor;
+            existingData.successMessage = req.body.successMessage ?? existingData.successMessage;
+            existingData.successColor = req.body.successColor ?? existingData.successColor;
+            existingData.failureMessage = req.body.failureMessage ?? existingData.failureMessage;
+            existingData.failureColor = req.body.failureColor ?? existingData.failureColor;
 
-        data.save()
-
-        res.status(200).send({ success: true, message: "zipCode value successfully save " })
-
-    }
-    catch (error) {
-        res.staus(500).send({ success: false, message: "internal server error" })
-    }
-}
-
-
-const update = async (req, res) => {
-    try {
-
-        const data = await ZipCode.findOne({ _id: req.params._id })
-        if (data) {
-            data.position = req.body.position ?? data.position;
-            data.heading = req.body.heading ?? data.heading;
-            data.icon = req.body.icon ?? data.icon;
-            data.placeholder = req.body.placeholder ?? data.placeholder;
-            data.borderStyle = req.body.borderStyle ?? data.borderStyle;
-            data.borderColor = req.body.borderColor ?? data.borderColor;
-            data.borderRadius = req.body.borderRadius ?? data.borderRadius;
-            data.buttonBgColor = req.body.buttonBgColor ?? data.buttonBgColor;
-            data.buttonTextColor = req.body.buttonTextColor ?? data.buttonTextColor;
-            data.successMessage = req.body.successMessage ?? data.successMessage;
-            data.successColor = req.body.successColor ?? data.successColor;
-            data.failureMessage = req.body.failureMessage ?? data.failureMessage;
-            data.failureColor = req.body.failureColor ?? data.failureColor;
-            console.log(data);
-            await data.save();
-            res.send({ success: true, message: "Record is Updated" });
+            await existingData.save();
+            res.status(200).send({ success: true, message: "Record successfully updated" });
         } else {
-
-            console.log("data not found");
-            res.status(404).send({ success: false, message: "Data not found" });
+         
+            const newData = new ZipCode(req.body);
+            await newData.save();
+            res.status(200).send({ success: true, message: "Record successfully created" });
         }
     } catch (error) {
         console.error(error);
         res.status(500).send({ success: false, message: "Internal Server Error" });
     }
-}
-module.exports = [create, update]
+};
+
+module.exports = createOrUpdate;
+
+// module.exports = [create,update]
--- Router/ZipCodeRouter.js
+++ Router/ZipCodeRouter.js
@@ -1,8 +1,17 @@
 const router = new require("express").Router()
-const [create,update] = require("../Controler/ZipCodeControler")
+// const [create,update] = require("../Controler/ZipCodeControler")
+// const createOrUpdate = require("../Controler/ZipCodeControler")
 
 
-router.post("/",create)
-router.put("/:_id",update)
+// router.post("/",create)
+// router.put("/:_id",update)
 
-module.exports = router
+// module.exports = router
+
+// const router = require("express").Router(); 
+const createOrUpdate = require("../Controler/ZipCodeControler"); 
+
+// Single POST route to handle both creation and updates
+router.post("/", createOrUpdate);
+
+module.exports = router;
